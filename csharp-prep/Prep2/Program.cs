c23f907e35649a94e77f8bf23135188e12c75b81
jguoliv <julloqueolivares@gmail.com>
2026-09-21 21:23:35 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        
+        string letterGrade = "";
+        
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "c";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations you passed the course!");
+        }
+        else
+        {
+            Console.Write("You have failed the class. Keep trying!"); 
+        }
     }
 }
